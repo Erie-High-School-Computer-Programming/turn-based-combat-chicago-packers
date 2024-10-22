9deb617dc9e00c03a50f766fc2c842f3f3aab630
JordanLovee <157502014+JordanLovee@users.noreply.github.com>
2024-10-22 13:33:48 +0000

    .

--- player.py
+++ player.py
@@ -1,17 +1,35 @@
 class Player:
-    def __init__(self):
-        """This method initializes the character
-        It should give the character a name, attack, defense, and health stat
-        It should also give the character a movelist"""
-        pass
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.hp = 100
+
+
 
     def attack(self, move, target):
         """This method should allow the character to attack another character using the 
         selected move. The move should deal damage to the target character"""
-        pass
+        target.hp -= move
+
+    move_list = {"Punch": {"damage": 10
+                            "accuracy": 50},
+                "Kick": 7,
+                "Body Slam": 15}
+    import random
+    if random.randint(0, 100) < move_list["Punch"]["accuracy"]:
+        Ernie.hp -= move_list["Punch"]["damage"]
+        print("Ernie was Punched")
+    else:
+        print("Attack missed")
 
     def display_stats(self):
         """This method should display the current health of the character"""
-        pass
+        
 
 
+p1 = Player("Peter Griffin", 100)
+p2 = Player("Ernie The Giant Chicken", 100)
+
+print(p2.hp)
+p1.attack(30, p2)
+print(p2.hp)
